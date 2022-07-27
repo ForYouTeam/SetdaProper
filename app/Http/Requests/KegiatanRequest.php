b5a99b3f6e44b8dbdb267e3c3da6735537c8d8f4
IPKIN9 <irwandirini@gmail.com>
2022-07-27 08:03:17 +0800

    Create: Request validation

---
+++ app/Http/Requests/KegiatanRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class KegiatanRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'nama_kegiatan' => 'required|min:2|max:255',
+            'tgl_mulai' => 'required|min:2|max:255|date',
+            'tgl_berakhir' => 'required|min:2|max:255|date',
+            'pakaian' => 'required|min:2|max:255',
+            'penjabat_menghadiri' => 'required|min:2|max:255',
+            'protokol' => 'required|integer',
+            'kopim' => 'required|integer',
+            'dokpim' => 'required|integer',
+            'keterangan' => 'required|min:2',
+            'calendar_id' => 'required|integer',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'response' => array(
+                'icon' => 'error',
+                'title' => 'Validasi Gagal',
+                'message' => 'Data yang di input tidak tervalidasi',
+            ),
+            'errors' => array(
+                'length' => count($validator->errors()),
+                'data' => $validator->errors()
+            ),
+        ], 422));
+    }
+}
---
+++ app/Http/Requests/PegawaiRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class PegawaiRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return false;
+    }
+
+    public function rules()
+    {
+        return [
+            'nama' => 'required|min:2|max:255',
+            'nip' => 'required|unique:pegawai,nip|numeric',
+            'jabatan' => 'required|min:2|max:255',
+            'bagian' => 'required|min:2|max:255',
+            'jk' => 'required|min:2|max:255'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'response' => array(
+                'icon' => 'error',
+                'title' => 'Validasi Gagal',
+                'message' => 'Data yang di input tidak tervalidasi',
+            ),
+            'errors' => array(
+                'length' => count($validator->errors()),
+                'data' => $validator->errors()
+            ),
+        ], 422));
+    }
+}
