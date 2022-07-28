c747ef813407ff9c39e59144afd7061f167d1232
IPKIN9 <irwandirini@gmail.com>
2022-07-29 06:53:59 +0800

    Modify: Logo Apps

--- resources/views/layout/SideBar.blade.php
+++ resources/views/layout/SideBar.blade.php
@@ -3,8 +3,8 @@
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
                 <div class="logo">
-                    <a href="#"><img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo"
-                            srcset=""></a>
+                    <a href="{{ route('dashboard.index') }}"><img style="height: 50px;"
+                            src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo" srcset=""></a>
                 </div>
                 <div class="toggler">
                     <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
